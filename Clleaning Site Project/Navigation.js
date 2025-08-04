6579b11cdf3c7c4499d0b7c8eda75f05bfb876b8
Tttt15676 <118089777+Tttt15676@users.noreply.github.com>
2025-08-03 22:11:02 -0500

    Delete Clleaning Site Project directory

--- Clleaning Site Project/Navigation.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const toggleButton = document.querySelector("#menu-toggle");
-    const navMenu = document.querySelector("#nav-menu");
-
-    toggleButton.addEventListener("click", function () {
-        navMenu.classList.toggle("active");
-        toggleButton.classList.toggle("open");
-    });
-});
